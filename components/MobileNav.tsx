08a530f981062664aac280dc99d51f342abbd6d5
glaucc <ahmedovyusif8785@gmail.com>
2024-05-26 20:08:52 +0400

    add mobile navbar

---
+++ components/MobileNav.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image'
+import Link from 'next/link'
+  
+
+const MobileNav = () => {
+  return (
+    <section className='w-full max-[264px]'>
+            <Sheet>
+                <SheetTrigger>
+                <Image 
+                    src="/icons/hamburger.svg"
+                    width={36}
+                    height={36}
+                    alt='hamburger icon'
+                    className='cursor-pointer sm:hidden'
+                />
+                </SheetTrigger>
+
+                <SheetContent side="left" className='border-none bg-dark-1'>
+                <Link href="/" className="flex items-center gap-1">
+                    <Image 
+                    src="/icons/logo.svg"
+                    width={32}
+                    height={32}
+                    alt='Prozum logo'
+                    className='max-sm:size-10'
+                    />
+                    <p className='text-[26px] font-extrabold text-white'>
+                    Prozum
+                    </p>
+                </Link>
+                </SheetContent>
+            </Sheet>
+
+        </section>
+  )
+}
+
+export default MobileNav
