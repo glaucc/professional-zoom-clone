5b77f621b1f40d0545e448e5465d0bcf3da5c396
glaucc <ahmedovyusif8785@gmail.com>
2024-05-28 13:24:18 +0400

    add web analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
